28e0718fa3d525b22fab398d2ae31eba38188903
Irina <irenserge@mail.ru>
2026-04-26 15:39:23 +0300

    feat: add pages checkout

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('dashboard')
+// checkout between projects  employees
+
+const projectsBtnEl = document.querySelector('#projects-btn');
+const emlpoyeesBtnEl = document.querySelector('#employees-btn');
+const projectsBlockEl = document.querySelector('#projects-block');
+const emlpoyeesBlockEl = document.querySelector('#employee-block');
+
+projectsBtnEl.addEventListener('click', () => {
+    emlpoyeesBtnEl.classList.remove('active');
+    projectsBtnEl.classList.add('active');
+    projectsBlockEl.classList.remove('hide');
+    emlpoyeesBlockEl.classList.add('hide');
+});
+emlpoyeesBtnEl.addEventListener('click', () => {
+    emlpoyeesBtnEl.classList.add('active');
+    projectsBtnEl.classList.remove('active');
+     projectsBlockEl.classList.add('hide');
+    emlpoyeesBlockEl.classList.remove('hide');
+});
